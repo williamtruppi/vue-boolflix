b822f81fea44cc5caeb1f23ec0a319bb51a45b96
William Truppi <williamt@hotmail.it>
2021-01-12 12:16:22 +0100

    Milestone 1 - quasi completa

--- milestone 1/assets/js/main.js
+++ milestone 1/assets/js/main.js
@@ -3,9 +3,25 @@ let app = new Vue ({
   el: ".wrapper",
 
   data: {
+    searchArray: [],
+    moviePoster: "",
     movieTitle: "",
     originalTitle: "",
     movieLanguage: "",
-    movieRating: 0
+    movieRating: null,
+    movieInput: "",
+  },
+
+  methods: {
+
+    searchMovies () {
+      console.log(this.movieInput);
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=3030b6d5014e4fc8b4997cc050050d0a&language=it-IT&query=${this.movieInput}`)
+      .then(response => {    
+        console.log(response.data.results);
+        this.searchArray = response.data.results;
+        console.log(this.searchArray);
+      })
+    }
   }
 })
